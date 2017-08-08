56284f28b223232f4488ec9e60e78c6b8eeac4ae
Levi Holmes <levi.holmes81@gmail.com>
2017-08-08 11:55:16 -0600

    add migrations

---
+++ migrations/20170808114824_create_cars.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("cars", (table) =>{
+    table.increments();
+    table.text("model");
+    table.text("make");
+    table.text("image");
+    table.integer("number_of_doors");
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable("cars");
+};
